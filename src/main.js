5d2ff41e967f81158861972a72678fd6d61f689c
D3v3l0per1 <manuel.widmoser12@gmail.com>
2019-06-26 10:57:20 +0200

    2nd Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+
+import Navbar from './components/Navbar'
+
 import HelloWorld from './components/HelloWorld'
 
+Vue.component('nav-bar', Navbar)
 Vue.component('helloWorld', HelloWorld)
 
 /* eslint-disable no-new */
